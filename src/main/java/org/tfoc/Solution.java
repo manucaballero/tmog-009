996ae816a75d26247615c204911940708a8ee718
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-03-14 08:53:50 +0100

    Solved Galadriel 009

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,102 @@
 package org.tfoc;
 
+import java.util.HashMap;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public Integer search(
-            Integer[] nums,
-            Integer target) {
+    private static final int ROTTEN_ORANGE = 2;
+    private static final int FRESH_ORANGE = 1;
 
-        return null;
+    public static Integer search(
+            Integer[][] nums) {
+
+        int totalMinutesElapsed = 0;
+        int freshOranges = 0;
+
+        for (Integer[] line : nums) {
+            for (Integer square : line) {
+
+                if (square == FRESH_ORANGE) {
+                    freshOranges++;
+                }
+            }
+        }
+
+        int previousMinuteFreshOranges = freshOranges;
+        boolean isPossibleToFinish = true;
+        HashMap<String, Boolean> visitedRottenOranges = new HashMap<>();
+        while (freshOranges > 0 && isPossibleToFinish){//Mientras queden naranjas por convertir y sean alcanzables
+
+            HashMap<String, Boolean> newRottenOranges = new HashMap<>();//Naranjas contaminadas en el minuto actual
+            for(int j = 0; j < nums.length; j++){
+                for(int k = 0; k < nums[j].length; k++){
+
+                    int currentSquare = nums[j][k];
+
+                    if(currentSquare == ROTTEN_ORANGE
+                            && newRottenOranges.get(j + "-" + k) == null
+                            && visitedRottenOranges.get(j + "-" + k) == null){
+                        //Si la casilla actual es una naranja contaminada que todavia no ha contaminado a otras
+                        //y no ha sido contaminada este minuto, entonces puede infectar a otras
+
+                        //Infectamos casilla superior
+                        if(j > 0 && k <= nums[j - 1].length){
+
+                            int aboveSquare = nums[j - 1][k];
+                            int aboveLine = j - 1;
+                            newRottenOranges.put(aboveLine + "-" + k, true);
+                            if(aboveSquare == FRESH_ORANGE){
+                                nums[j - 1][k] = 2;
+                                freshOranges--;
+                            }
+                        }
+                        //Infectamos casilla inferior
+                        if(j < nums.length - 1  && k <= nums[j + 1].length){
+
+                            int belowSquare = nums[j + 1][k];
+                            int belowLine = j + 1;
+                            newRottenOranges.put(belowLine + "-" + k, true);
+                            if(belowSquare == FRESH_ORANGE){
+                                nums[j + 1][k] = 2;
+                                freshOranges--;
+                            }
+                        }
+                        //Infectamos casilla izquierda
+                        if(k > 0){
+
+                            int leftSquare = nums[j][k - 1];
+                            int leftColumn = k - 1;
+                            newRottenOranges.put(j + "-" + leftColumn, true);
+                            if(leftSquare == FRESH_ORANGE){
+                                nums[j][k - 1] = 2;
+                                freshOranges--;
+                            }
+                        }
+                        //Infectamos casilla derecha
+                        if(k < nums[j].length - 1){
+
+                            int rightSquare = nums[j][k + 1];
+                            int rightColumn = k + 1;
+                            newRottenOranges.put(j + "-" + rightColumn, true);
+                            if(rightSquare == FRESH_ORANGE){
+                                nums[j][k + 1] = 2;
+                                freshOranges--;
+                            }
+                        }
+                        visitedRottenOranges.put(j + "-" + k, true);
+                    }
+                }
+            }
+            if(previousMinuteFreshOranges == freshOranges){
+                isPossibleToFinish = false;
+            }
+            previousMinuteFreshOranges = freshOranges;
+            totalMinutesElapsed++;
+        }
+        return freshOranges == 0 ? totalMinutesElapsed : -1;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,64 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void example01(){
+        //grid = [[2,1,1],[1,1,0],[0,1,1]]
+        // 2 1 1
+        // 1 1 0
+        // 0 1 1
+        Integer[][] grid = new Integer[3][3];
+        grid[0][0] = 2;
+        grid[0][1] = 1;
+        grid[0][2] = 1;
+        grid[1][0] = 1;
+        grid[1][1] = 1;
+        grid[1][2] = 0;
+        grid[2][0] = 0;
+        grid[2][1] = 1;
+        grid[2][2] = 1;
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        assertEquals(4, Solution.search(grid));
+    }
+
+    @Test
+    void example02(){
+        //grid = [[2,1,1],[0,1,1],[1,0,1]]
+        // 2 1 1
+        // 0 1 1
+        // 1 0 1
+
+        //Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+        Integer[][] grid = new Integer[3][3];
+        grid[0][0] = 2;
+        grid[0][1] = 1;
+        grid[0][2] = 1;
+        grid[1][0] = 0;
+        grid[1][1] = 1;
+        grid[1][2] = 1;
+        grid[2][0] = 1;
+        grid[2][1] = 0;
+        grid[2][2] = 1;
+
+        assertEquals(-1, Solution.search(grid));
+    }
+
+    @Test
+    void example03(){
+        //grid = [[0,2]]
+        // 0 2
+
+        Integer[][] grid = new Integer[1][2];
+        grid[0][0] = 0;
+        grid[0][1] = 2;
+
+        assertEquals(0, Solution.search(grid));
     }
 
 }
